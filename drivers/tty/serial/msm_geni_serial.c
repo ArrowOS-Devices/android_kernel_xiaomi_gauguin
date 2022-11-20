32be8bb79fece73f5fe5da378165b44376903340
hridaya <hridaya@pixelos.net>
2022-11-20 12:35:27 +0545

    drivers/tty: serial: Silence ipc log spam after disabling ipc logging
     - Silences following spam:
    
    [    0.557446] msm_geni_serial 884000.qcom,qup_uart: Err in Rx IPC Log
    [    0.557448] msm_geni_serial 884000.qcom,qup_uart: Err in Tx IPC Log
    [    0.557451] msm_geni_serial 884000.qcom,qup_uart: Err in Pwr IPC Log
    [    0.557453] msm_geni_serial 884000.qcom,qup_uart: Err in Misc IPC Log
    [    0.557455] msm_geni_serial 884000.qcom,qup_uart: Err in single IPC Log
    [    0.557457] msm_geni_serial 884000.qcom,qup_uart: Err in irqstatus IPC Log
    
    Signed-off-by: hridaya <hridaya@pixelos.net>
    Change-Id: I7e02ec865226a97b242ac880f476a8ec4c3aa48f

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -3137,7 +3137,7 @@ static void msm_geni_serial_debug_init(struct uart_port *uport, bool console)
 			msm_port->ipc_log_rx = ipc_log_context_create(
 					IPC_LOG_TX_RX_PAGES, name, 0);
 			if (!msm_port->ipc_log_rx)
-				dev_info(uport->dev, "Err in Rx IPC Log\n");
+				dev_dbg(uport->dev, "Err in Rx IPC Log\n");
 		}
 		memset(name, 0, sizeof(name));
 		if (!msm_port->ipc_log_tx) {
@@ -3146,7 +3146,7 @@ static void msm_geni_serial_debug_init(struct uart_port *uport, bool console)
 			msm_port->ipc_log_tx = ipc_log_context_create(
 					IPC_LOG_TX_RX_PAGES, name, 0);
 			if (!msm_port->ipc_log_tx)
-				dev_info(uport->dev, "Err in Tx IPC Log\n");
+				dev_dbg(uport->dev, "Err in Tx IPC Log\n");
 		}
 		memset(name, 0, sizeof(name));
 		if (!msm_port->ipc_log_pwr) {
@@ -3155,7 +3155,7 @@ static void msm_geni_serial_debug_init(struct uart_port *uport, bool console)
 			msm_port->ipc_log_pwr = ipc_log_context_create(
 					IPC_LOG_PWR_PAGES, name, 0);
 			if (!msm_port->ipc_log_pwr)
-				dev_info(uport->dev, "Err in Pwr IPC Log\n");
+				dev_dbg(uport->dev, "Err in Pwr IPC Log\n");
 		}
 		memset(name, 0, sizeof(name));
 		if (!msm_port->ipc_log_misc) {
@@ -3164,7 +3164,7 @@ static void msm_geni_serial_debug_init(struct uart_port *uport, bool console)
 			msm_port->ipc_log_misc = ipc_log_context_create(
 					IPC_LOG_MISC_PAGES, name, 0);
 			if (!msm_port->ipc_log_misc)
-				dev_info(uport->dev, "Err in Misc IPC Log\n");
+				dev_dbg(uport->dev, "Err in Misc IPC Log\n");
 		}
 		/* New set of UART IPC log for RX Invalid case */
 		memset(name, 0, sizeof(name));
@@ -3174,7 +3174,7 @@ static void msm_geni_serial_debug_init(struct uart_port *uport, bool console)
 			msm_port->ipc_log_single = ipc_log_context_create(
 					IPC_LOG_MISC_PAGES, name, 0);
 			if (!msm_port->ipc_log_single)
-				dev_info(uport->dev, "Err in single IPC Log\n");
+				dev_dbg(uport->dev, "Err in single IPC Log\n");
 		}
 		memset(name, 0, sizeof(name));
 		if (!msm_port->ipc_log_irqstatus) {
@@ -3183,7 +3183,7 @@ static void msm_geni_serial_debug_init(struct uart_port *uport, bool console)
 			msm_port->ipc_log_irqstatus = ipc_log_context_create(
 					IPC_LOG_MISC_PAGES, name, 0);
 			if (!msm_port->ipc_log_irqstatus)
-				dev_info(uport->dev, "Err in irqstatus IPC Log\n");
+				dev_dbg(uport->dev, "Err in irqstatus IPC Log\n");
 		}
 	} else {
 		memset(name, 0, sizeof(name));
