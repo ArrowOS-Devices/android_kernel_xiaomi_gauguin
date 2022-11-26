69884154c37aac3f142981bbc2459c1168e2452f
hridaya <hridaya@pixelos.net>
2022-11-26 13:28:32 +0545

    include/drm: drm_notifier_mi: Import xiaomi modifications
     * from munch-s-oss
    
    Signed-off-by: hridaya <hridaya@pixelos.net>
    Change-Id: I30b8f5ac5f376f23c468bf88a7681bc086dbc0ae

--- include/drm/drm_notifier_mi.h
+++ include/drm/drm_notifier_mi.h
@@ -38,14 +38,18 @@ enum msm_drm_display_id {
 #define MI_DRM_EVENT_BLANK			0x01
 /* A hardware display blank early change occurred */
 #define MI_DRM_EARLY_EVENT_BLANK		0x02
+/* A hardware display blank more early change occured */
+#define MI_DRM_PRE_EVENT_BLANK          	0x03
 
 enum {
-	MI_DRM_BLANK_UNBLANK = 0,
-	MI_DRM_BLANK_LP1,
-	MI_DRM_BLANK_LP2,
-	MI_DRM_BLANK_STANDBY,
-	MI_DRM_BLANK_SUSPEND,
-	MI_DRM_BLANK_POWERDOWN,
+	/* panel: power on */
+	MI_DRM_BLANK_UNBLANK   = 0,
+	MI_DRM_BLANK_LP1       = 1,
+	MI_DRM_BLANK_LP2       = 2,
+	MI_DRM_BLANK_STANDBY   = 3,
+	MI_DRM_BLANK_SUSPEND   = 4,
+	/* panel: power off */
+	MI_DRM_BLANK_POWERDOWN = 5,
 };
 
 struct mi_drm_notifier {
