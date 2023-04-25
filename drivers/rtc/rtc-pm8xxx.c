9741c00ed68bd20d0843f34ce236ddefa583e540
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-04-26 02:29:57 +0600

    Revert "rtc: control default alarm wake up capability"
    Our RTC peripheral in PM8941 PMIC does not support disabling RTC alarm wakeup.
    This property is enabled only on msm-5.4 and msm-5.15 at the moment for iot
    and vm platforms which indicates that it is clearly not for our SDM660 mobile
    platform. We are never going to enable "disable-alarm-wakeup" at dts. So, drop
    this useless patch.
    
    This reverts commit 0ee97ccec85bcfa0b268038439e8dc8a952522e9.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/rtc/rtc-pm8xxx.c
+++ drivers/rtc/rtc-pm8xxx.c
@@ -424,9 +424,6 @@ static int pm8xxx_rtc_probe(struct platform_device *pdev)
 	if (rc < 0)
 		return rc;
 
-	if (of_property_read_bool(pdev->dev.of_node, "disable-alarm-wakeup"))
-		device_set_wakeup_capable(&pdev->dev, false);
-
 	return rtc_register_device(rtc_dd->rtc);
 }
 
